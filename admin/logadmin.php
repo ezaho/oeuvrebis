79466d218f2056bc3bb9736840dc614e3269f27e
ezaho <claudyvia@coditel.net>
2017-12-22 11:24:48 +0100

    admin

---
+++ admin/logadmin.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+?>
+<form name = "admin" methode = "post" action = "">
+   <label pour = 'title'> ADMIN : </label>  <br/>
+        	 
+       <input type="pseudo" name="pseudo" placeholder="pseudo"/>  <br>        
+       <input type="password" name="mdp" placeholder="password"/>  <br>
+       <input type="submit" name="formlog" value="Me connecter en tant qu'administrateur"/>
+   
+ </form > 
